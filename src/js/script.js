dc0a3c42b7c2390c556466b02123768b6225acc3
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-03-13 08:29:26 +0100

    Correct code format

--- src/js/script.js
+++ src/js/script.js
@@ -1,68 +1,69 @@
 {
-    'use strict';
+  ('use strict');
 
-    const select = {
-      templateOf: {
-        bookProduct: '#template-book',
-        },
-      containerOf: {
-        booksList: '.books-list',
-        },
-      element:{
-        bookImage: '.book__image',
-        }
+  const select = {
+    templateOf: {
+      bookProduct: '#template-book',
+    },
+    containerOf: {
+      booksList: '.books-list',
+    },
+    element: {
+      bookImage: '.book__image',
+    },
+  };
+  const templates = {
+    bookProduct: Handlebars.compile(
+      document.querySelector(select.templateOf.bookProduct).innerHTML
+    ),
+  };
+
+  const render = function () {
+    const thisBook = this;
+
+    for (let book of dataSource.books) {
+      const generatedHTML = templates.bookProduct(book);
+
+      thisBook.element = utils.createDOMFromHTML(generatedHTML);
+
+      const bookContainer = document.querySelector(
+        select.containerOf.booksList
+      );
+
+      bookContainer.appendChild(thisBook.element);
     }
-    const templates = {
-        bookProduct: Handlebars.compile(document.querySelector(select.templateOf.bookProduct).innerHTML),
-    }
+  };
 
-    const render = function () {
-        const thisBook = this;
+  const favoriteBooks = [];
 
-        for (let book of dataSource.books) {
-    
-          const generatedHTML = templates.bookProduct(book);
-    
-          thisBook.element = utils.createDOMFromHTML(generatedHTML);
-    
-          const bookContainer = document.querySelector(select.containerOf.booksList);
-    
-          bookContainer.appendChild(thisBook.element);
-        }
-      };
+  const initActions = function () {
+    const thisBook = this;
 
-    const favoriteBooks =[];
+    thisBook.accordionTrigger = document.querySelector(
+      select.containerOf.booksList
+    );
+    thisBook.bookImage = thisBook.accordionTrigger.querySelectorAll('.book a');
 
-    const initActions = function (){
+    thisBook.accordionTrigger.addEventListener('dblclick', function (event) {
+      event.preventDefault();
+      const book = event.target.offsetParent;
+      console.log('clicked', book);
 
-        const thisBook = this;
-
-        thisBook.accordionTrigger  = document.querySelector(select.containerOf.booksList);
-        thisBook.bookImage = thisBook.accordionTrigger.querySelectorAll('.book a');           
-      
-        thisBook.accordionTrigger .addEventListener('dblclick', function(event){
-            event.preventDefault();
-            const book = event.target.offsetParent;
-            console.log('clicked', book);
-
-      if(book.classList.contains('book__image')){       
-       
+      if (book.classList.contains('book__image')) {
         book.classList.toggle('favorite');
 
-          const bookImageId = book.getAttribute('data-id');
+        const bookImageId = book.getAttribute('data-id');
 
-          if(favoriteBooks.includes(bookImageId)){
-            favoriteBooks.splice(favoriteBooks.indexOf(bookImageId));
-          }
-          else {
-            favoriteBooks.push(bookImageId);
-          }         
+        if (favoriteBooks.includes(bookImageId)) {
+          favoriteBooks.splice(favoriteBooks.indexOf(bookImageId));
+        } else {
+          favoriteBooks.push(bookImageId);
+        }
       }
       console.log('favoriteBooks', favoriteBooks);
     });
-}    
+  };
 
-    render();
-    initActions();
-
+  render();
+  initActions();
 }
